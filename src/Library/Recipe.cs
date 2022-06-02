aaf0eceeb2eb42e9e1eecb144ea12babbda208dc
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-06-02 19:31:01 -0300

    trabajo finalizado

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -15,9 +15,12 @@ namespace Full_GRASP_And_SOLID
 
         public Product FinalProduct { get; set; }
 
-        public void AddStep(Step step)
+        //Se crea la instancia de step dentro del metodo AddStep ya que esta es experta en crear las instancias porque conoce todo para hacerlo.
+        public Step AddStep(Product input, double quantity, Equipment equipment, int time)
         {
+            Step step = new Step(input, quantity, equipment, time);
             this.steps.Add(step);
+            return step;
         }
 
         public void RemoveStep(Step step)
